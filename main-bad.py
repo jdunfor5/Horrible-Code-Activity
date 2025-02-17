6be9e05923d4c4dabe3adbcb669b7bf86a11e5d6
Ailani Borges Lopez <ailanimbl@gmail.com>
2025-02-17 15:30:40 -0500

    feat: bad code

---
+++ main-bad.py
@@ -0,0 +1,26 @@
+def calculate(num1, num2, calculation):
+    total = 0 #sets the total to 0
+
+    if calculation == "+":
+        total = total + num1 #adds num1 to total
+        total = total + num2 #adds num2 to total
+
+    elif calculation == "-":
+        total = num1
+        total = total - num2
+
+    elif calculation == "/":
+        total = num1
+        total = total / num2
+
+    elif calculation == "*":
+        total = num1
+        total = total * num2
+    else:
+        return "Issue has occurred"
+    return total
+
+print(calculate(1, 2, "+"))
+print(calculate(1, 2, "-"))
+print(calculate(1, 2, "/"))
+print(calculate(1, 2, "*"))
