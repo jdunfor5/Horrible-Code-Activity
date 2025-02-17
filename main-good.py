ddc49c67f63c62659db6956bd38852d8cd113b30
Jacob <jdunfor5@charlotte.edu>
2025-02-17 18:22:06 -0500

    Add multiply function and general documentation

--- main-good.py
+++ main-good.py
@@ -1,8 +1,17 @@
+# Adds the numbers collected from the user
 def add():
+    pass
 
+# Subtracts the numbers collected from the user
 def subtract():
+    pass
 
-def multiply():
+# Multiplies the numbers collected from the user
+def multiply(num1, num2):
+    return num1 * num2
 
+# Divides the numbers collected from the user
 def divide():
+    pass
 
+# Main
